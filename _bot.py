ff7b4713ca0c83c8b0be8e61f0bcb4f4f17c9482
Vinay Kumar <vpal93592@gmail.com>
2026-01-03 13:20:04 +0530

    Update _bot.py

--- _bot.py
+++ _bot.py
@@ -2,25 +2,32 @@ import pyautogui
 import time
 import pyperclip
 from openai import OpenAI
+
 client = OpenAI(
   api_key="<Your Key Here>",
 )
-def is_last_message_from_sender(chat_log, sender_name="Rohan 2nd"):
+
+def last_message_from_sender(whatsapp_chat_text, sender_name="Rohan 2nd"):
     messages = chat_log.strip().split("/2025] ")[-1]
     if sender_name in messages:
         return True 
     return False
+
 pyautogui.click(1040, 1050)
 time.sleep(1)
+
 while True:
  pyautogui.moveTo(699,186)
  pyautogui.dragTo(1107,931,duration=1.0,button='left')
  pyautogui.hotkey('ctrl','c')
  time.sleep(2)
- pyautogui.click(913,795)
- chat_history = pyperclip.paste()
- print(chat_history)
- if is_last_message_from_sender(chat_history):
+ 
+  pyautogui.click(913,795)
+ 
+  chat_history = pyperclip.paste()
+  print(chat_history)
+ 
+  if is_last_message_from_sender(chat_history):
      completion = client.chat.completions.create(
      model="gpt-3.5-turbo",
      messages=[
@@ -30,6 +37,7 @@ while True:
       ]
    )
     response = completion.choices[0].message.content
+    
     pyperclip.copy(response)
     pyautogui.click(885, 965)
     time.sleep(1)  
@@ -38,3 +46,4 @@ while True:
     pyautogui.press('enter')
 
         pyautogui.press('enter')
+
